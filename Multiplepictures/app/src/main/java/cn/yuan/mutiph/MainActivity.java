fcf897b85a069f18bed51e7ac8e2f0bd298d8fa6
于阔源 <yukuoyuan@hotmail.com>
2017-03-03 13:35:08 +0800

    提交了修改了

--- Multiplepictures/app/src/main/java/cn/yuan/mutiph/MainActivity.java
+++ Multiplepictures/app/src/main/java/cn/yuan/mutiph/MainActivity.java
@@ -1,16 +1,23 @@
 package cn.yuan.mutiph;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
 
 import cn.yuan.pt.activity.MultiplePicturesActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Intent intent;
+    private ImageView iv_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +25,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button bt_start_one = (Button) findViewById(R.id.bt_start_one);
         Button bt_start_more = (Button) findViewById(R.id.bt_start_more);
+        iv_test = (ImageView) findViewById(R.id.iv_test);
         bt_start_one.setOnClickListener(this);
         bt_start_more.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 0x001) {
+                if (data != null) {
+                    ArrayList<String> list = data.getStringArrayListExtra("select_result");
+                    Toast.makeText(MainActivity.this, list.size() + "", Toast.LENGTH_LONG).show();
+                    Glide.with(MainActivity.this).load(list.get(0)).into(iv_test);
+                }
+            }
+        }
     }
 
     @Override
@@ -27,7 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.bt_start_one:
                 intent = new Intent(this, MultiplePicturesActivity.class);
-                intent.putExtra("number", 0);
+                intent.putExtra("number", 1);
                 startActivityForResult(intent, 0x001);
                 break;
             case R.id.bt_start_more:
